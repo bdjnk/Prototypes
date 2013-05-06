fee220c7b643277576d81feb8b23924390942e93
Michael Plotke <michaelplotke@gmail.com>
2013-05-06 13:14:22 -0700

    Tweaked: menu, controller, turret prototype

--- Assets/Resources/Scripts/PrototypeMenu.cs
+++ Assets/Resources/Scripts/PrototypeMenu.cs
@@ -5,27 +5,29 @@ public class PrototypeMenu : MonoBehaviour {
 
 	void OnGUI()
 	{
-		if (GUI.Button(new Rect(0, 0, Screen.width, 50), "Basic Controls"))
+		float height = Screen.height / 6.0f;
+		
+		if (GUI.Button(new Rect(0, height*0, Screen.width, height), "Basic Controls"))
 		{
 			Application.LoadLevel("Basics");
 		}
-		if (GUI.Button(new Rect(0, 50, Screen.width, 50), "Level (Map) Auto Generation"))
+		if (GUI.Button(new Rect(0, height*1, Screen.width, height), "Level (Map) Auto Generation"))
 		{
 			Application.LoadLevel("AutoMap");
 		}
-		if (GUI.Button(new Rect(0, 100, Screen.width, 50), "Auto Tracking Turret Bot"))
+		if (GUI.Button(new Rect(0, height*2, Screen.width, height), "Auto Tracking Turret Bot"))
 		{
 			Application.LoadLevel("TurretBot");
 		}
-		if (GUI.Button(new Rect(0, 150, Screen.width, 50), "Multiplayer Networking"))
+		if (GUI.Button(new Rect(0, height*3, Screen.width, height), "Multiplayer Networking"))
 		{
 			Application.LoadLevel("Networking");
 		}
-		if (GUI.Button(new Rect(0, 200, Screen.width, 50), "Upgrades"))
+		if (GUI.Button(new Rect(0, height*4, Screen.width, height), "Upgrades"))
 		{
 			Application.LoadLevel("Upgrades");
 		}
-		if (GUI.Button(new Rect(0, 250, Screen.width, 50), "Lighting"))
+		if (GUI.Button(new Rect(0, height*5, Screen.width, height), "Lighting"))
 		{
 			Application.LoadLevel("Lighting");
 		}
